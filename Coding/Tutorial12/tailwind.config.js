0daf6651f399266fdcf6c31ecb51eadd6987661b
AllahRakha1234 <allahrakhahsp1234@gmail.com>
2024-08-11 23:42:33 +0500

    initialization

---
+++ Coding/Tutorial12/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing: {
+        15: "15px"
+      }
+    },
+  },
+  plugins: [],
+}
+
